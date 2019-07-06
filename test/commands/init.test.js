dab5e008c76f2ee6f5cdc7567ced520970bcdfe9
David Hérault <dherault@gmail.com>
2019-07-06 13:48:00 +0200

    Comment out tests

---
+++ test/commands/init.test.js
@@ -0,0 +1,17 @@
+// const { describe, test, expect } = require('@oclif/test')
+
+// describe('hello', () => {
+  // test
+  // .stdout()
+  // .command(['hello'])
+  // .it('runs hello', ctx => {
+  //   expect(ctx.stdout).to.contain('hello world')
+  // })
+
+  // test
+  // .stdout()
+  // .command(['hello', '--name', 'jeff'])
+  // .it('runs hello --name jeff', ctx => {
+  //   expect(ctx.stdout).to.contain('hello jeff')
+  // })
+// })
